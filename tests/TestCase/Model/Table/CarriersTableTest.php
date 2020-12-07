63cf448e43ad9a81a7c50402fa0bb926a1036431
edwin66627 <edwinqs27@gmail.com>
2020-12-07 16:18:08 -0500

    Carriers controller, model and templates added

---
+++ tests/TestCase/Model/Table/CarriersTableTest.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\CarriersTable;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Table\CarriersTable Test Case
+ */
+class CarriersTableTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Table\CarriersTable
+     */
+    protected $Carriers;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    protected $fixtures = [
+        'app.Carriers',
+        'app.Patients',
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $config = $this->getTableLocator()->exists('Carriers') ? [] : ['className' => CarriersTable::class];
+        $this->Carriers = $this->getTableLocator()->get('Carriers', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        unset($this->Carriers);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
